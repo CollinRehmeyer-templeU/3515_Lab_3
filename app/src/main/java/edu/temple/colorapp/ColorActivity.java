8e2498f51ce3bf693db02b684356fe2361a77b28
CollinRehmeyer-templeU <tuk60980@temple.edu>
2020-09-28 17:50:21 -0400

    Background color changes when a view is selected from the spinner, and the background color of the selected view turns white

--- app/src/main/java/edu/temple/colorapp/ColorActivity.java
+++ app/src/main/java/edu/temple/colorapp/ColorActivity.java
@@ -2,6 +2,7 @@ package edu.temple.colorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,16 +17,19 @@ public class ColorActivity extends AppCompatActivity {
 
 
         Spinner mySpinner = findViewById(R.id.spinner);
-        View myBackground = findViewById(R.id.background);
+        final View myBackground = findViewById(R.id.background);
         String[] myColors = {"RED", "BLUE", "YELLOW", "GREEN", "AQUA", "PURPLE", "GREY", "MAGENTA", "CYAN", "WHITE"};
-        ColorAdapter myColorAdapter = new ColorAdapter(getApplicationContext(), myColors);
+        final ColorAdapter myColorAdapter = new ColorAdapter(getApplicationContext(), myColors);
         mySpinner.setAdapter(myColorAdapter);
 
         mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int newColor = Color.parseColor(myColorAdapter.getItem(position).toString());
+                myBackground.setBackgroundColor(newColor);
 
+                view.setBackgroundColor(Color.WHITE);
             }
 
             @Override
